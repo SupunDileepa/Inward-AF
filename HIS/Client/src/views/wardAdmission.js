08926158ae9c4bed70484c7411f60358402f9ce5
nirmalrv8 <nirmal.seneviratne@gmail.com>
2018-06-12 19:45:34 +0530

    added ward admision and searchByBHT pages.
    just routes and all.

---
+++ HIS/Client/src/views/wardAdmission.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class WardAdmission extends Component {
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <button>view BHT</button>
+      </div>
+    )
+  }
+}
+
+export default WardAdmission;
