d7372449118f3ed6b383d4f30400c89cfe890ce1
nirmalrv8 <nirmal.seneviratne@gmail.com>
2018-06-12 20:13:21 +0530

    added contents
    to ward admissions and searchByBHT

--- HIS/Client/src/views/wardAdmission.js
+++ HIS/Client/src/views/wardAdmission.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
 
+
 class WardAdmission extends Component {
   render() {
     return (
-      <div className="animated fadeIn">
-        <button>view BHT</button>
+      <div className="animated fadeIn">        
+        
+        <table border="true">
+            <tr>
+                <th>BHT Number</th>
+                <th>Patient Name</th>
+            </tr>
+            <tr>
+                <td>20181</td>
+                <td>Maria Anders</td>
+                <td><button>view BHT</button></td>
+            </tr>
+            <tr>
+                <td>20182</td>
+                <td>John Lenin</td>
+                <td><button>view BHT</button></td>
+            </tr>
+        </table>
+        
       </div>
     )
   }
