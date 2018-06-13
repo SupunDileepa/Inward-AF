7439b54d2034e14465e222bcaad62aa5374724d8
nirmalrv8 <nirmal.seneviratne@gmail.com>
2018-06-13 15:06:36 +0530

    opening bht manager in new link

--- HIS/Client/src/views/wardAdmission.js
+++ HIS/Client/src/views/wardAdmission.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 
 
 class WardAdmission extends Component {
+
+  gotoBhtFull(){
+    window.open('/#/bht' + '20181','_blank');
+  }
+
   render() {
     return (
       <div className="animated fadeIn">        
@@ -9,15 +14,18 @@ class WardAdmission extends Component {
         <table border="true">
             <tr>
                 <th>BHT Number</th>
+                <th>PID</th>
                 <th>Patient Name</th>
             </tr>
             <tr>
                 <td>20181</td>
+                <td>1</td>
                 <td>Maria Anders</td>
-                <td><button>view BHT</button></td>
+                <td><button onClick={this.gotoBhtFull.bind(this)}>view BHT</button></td>
             </tr>
             <tr>
                 <td>20182</td>
+                <td>2</td>
                 <td>John Lenin</td>
                 <td><button>view BHT</button></td>
             </tr>
