d664cb4006cc087ba596b431ca4917932351123a
nirmalrv8 <nirmal.seneviratne@gmail.com>
2018-06-14 01:03:03 +0530

    serach by bht - only mapping is left.

--- HIS/Client/src/views/searchByBHT.js
+++ HIS/Client/src/views/searchByBHT.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
 
 class SearchByBHT extends Component {
+
+  search(){
+    // check whether the field is empty
+    if(!this.refs.bht.value) return;    
+
+    fetch('http://localhost:5000/api/patients/' + this.refs.bht.value)
+    .then((response) => {
+      // check whether the response is empty
+      if(response.headers.get("Content-Length") == 2) {
+        alert("No patient found with the given BHT number");
+        return;
+      }
+      else{
+        console.log(response.json());
+      }
+    })
+  }  
+
   render() {
     return (
-      <div className="animated fadeIn">        
-        <input type="text" placeholder="Patient BHT ID"/>
-        <button>Search</button>
+      <div className="animated fadeIn"> 
+      <form>
+        <input type="number" ref="bht" placeholder="Patient BHT ID" required/>
+        <input type="submit" onClick={this.search.bind(this)} Search/>
+      </form>
       </div>
     )
   }
