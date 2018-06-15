39d3594f411e54f72d238bd36ea1f203f735eae8
Supun Dileepa <dilee1st.1995@gmail.com>
2018-06-15 18:57:24 +0530

    comiited

--- HIS/Client/src/components/PatientProfile/PatientProfile.js
+++ HIS/Client/src/components/PatientProfile/PatientProfile.js
@@ -6,6 +6,7 @@ import PatientDetails from "./PatientDetails";
 class PatientProfile extends Component {
   constructor(props) {
     super(props);
+    //console.log(this.state.patients);
     this.state = {
       p: JSON.parse(localStorage.getItem("patientDetails")),
       patients: []
@@ -15,38 +16,31 @@ class PatientProfile extends Component {
   componentDidMount() {
     console.log(this.state.p.pid);
     axios
-      .get(`http://localhost:5000/api/patients/${this.state.p.pid}`)
+      .get(`http://localhost:5000/api/patients/9`)
       .then(res => {
         this.setState({ patients: res.data });
-        //console.log(this.state.patients);
+        console.log(this.state.patients);
       })
       .catch(err => {
         console.log(err);
       });
   }
+  // ${this.state.p.pid}
 
   render() {
-    var arr = this.state.patients.obj;
-    var patientDetails = "";
-    console.log(arr);
-    if (arr) {
-      patientDetails = arr.map((patient, i) => {
-        console.log(patient);
-        return (
-          <PatientDetails
-            key={i}
-            pId={patient.pId}
-            gender={patient.gender}
-            name={patient.name}
-            dob={patient.dob}
-          />
-        );
-      });
-    } else {
-      patientDetails = "";
-      return patientDetails;
-    }
+    // var arr = this.state.patients.obj;
+    // var patientDetails = "";
+    // console.log(arr);
+    //if (arr) {
+    // patientDetails = arr.map((patient, i) => {
+    //   console.log(patient);
 
+    //   });
+    // } else {
+    //   patientDetails = "";
+    //   return patientDetails;
+    // }
+    console.log(this.state.patients.obj);
     return (
       <div className="container">
         <h4>Patient Details</h4>
@@ -60,7 +54,14 @@ class PatientProfile extends Component {
               <th>Date Of Birth</th>
             </tr>
           </thead>
-          <tbody>{patientDetails}</tbody>
+          <tbody>
+            <tr>
+              {/* <td>{this.state.patients.obj.pId}</td> */}
+              <td>{this.state.patients.obj.gender}</td>
+              <td>{this.state.patients.name}</td>
+              {/* <td>{this.props.dob}</td> */}
+            </tr>
+          </tbody>
         </Table>
       </div>
     );
