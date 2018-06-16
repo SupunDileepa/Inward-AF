c3d4640ab7e46864ee614658b8cac6162efafa02
Supun Dileepa <dilee1st.1995@gmail.com>
2018-06-16 14:54:17 +0530

    Spring Final Changes

--- HIS/Client/src/views/download.js
+++ HIS/Client/src/views/download.js
@@ -1,11 +1,8 @@
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
-
-import React, {Component, PropTypes} from 'react';
-import Viewtest from './LabTests/viewTest';
-
-
+import React, { Component, PropTypes } from "react";
+import Viewtest from "./LabTests/viewTest";
 
 export default class Export extends Component {
   constructor(props) {
@@ -13,38 +10,41 @@ export default class Export extends Component {
   }
 
   printDocument() {
-    const input = document.getElementById('divToPrint');
-    html2canvas(input)
-      .then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF();
-        pdf.addImage(imgData, 'JPEG', 0, 0);
-        // pdf.output('dataurlnewwindow');
-        pdf.save("download.pdf");
-      })
-    ;
+    const input = document.getElementById("divToPrint");
+    html2canvas(input).then(canvas => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "JPEG", 0, 0);
+      // pdf.output('dataurlnewwindow');
+      pdf.save("download.pdf");
+    });
   }
 
   render() {
     return (
       <div>
-      <div className="mb5">
-        <button className="btn btn-primary" onClick={this.printDocument}>Get Report</button>
+        <div className="mb5">
+          <button className="btn btn-primary" onClick={this.printDocument}>
+            Get Report
+          </button>
+        </div>
+
+        <div
+          id="divToPrint"
+          className="mt4"
+          style={{
+            backgroundColor: "#f5f5f5",
+            width: "180mm",
+            minHeight: "297mm",
+            marginLeft: "auto",
+            marginRight: "auto"
+          }}
+        >
+          <div id="divToPrint">
+            <Viewtest />
+          </div>
+        </div>
       </div>
-
-       <div id="divToPrint" className="mt4" style={{   backgroundColor : '#f5f5f5',
-        width : '220mm',
-        minHeight : '297mm',
-        marginLeft :'auto',
-        marginRight :'auto'}}
-     
-      > 
-
-             
-                      
-              
-        <div id="divToPrint" ><Viewtest/></div>
-       </div> 
-    </div>);
+    );
   }
 }
